877ae3af91a74fb4073a41cc3863a830cb1da283
gethertv <gethertv@gmail.com>
2023-10-09 14:54:33 +0200

    beta 0.1

--- src/main/java/dev/gether/anonymouse/Anonymouse.java
+++ src/main/java/dev/gether/anonymouse/Anonymouse.java
@@ -1,17 +1,69 @@
 package dev.gether.anonymouse;
 
+import dev.gether.anonymouse.rest.DemoRestController;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
-public final class Anonymouse extends JavaPlugin {
+import java.util.concurrent.TimeUnit;
 
+public class Anonymouse extends JavaPlugin {
+
+    private Server server;
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // config.yml
+        saveDefaultConfig();
 
+        // init rest api
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            try {
+                initRestApi();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        try {
+            if (server != null && server.isRunning()) {
+                server.stop();
+                getLogger().info("Server stopped!");
+            }
+        } catch (Exception ignored) {}
     }
+
+    private void initRestApi() throws Exception {
+        // creating server with port
+        server = new Server();
+
+        ServerConnector serverConnector = new ServerConnector(server);
+        serverConnector.setIdleTimeout(TimeUnit.HOURS.toMillis(1));
+        serverConnector.setHost(getConfig().getString("server.host", "127.0.0.1"));
+        serverConnector.setPort(getConfig().getInt("server.port", 8080));
+        server.addConnector(serverConnector);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+
+        // routes
+        ServletHolder servletHolder = new ServletHolder(new DemoRestController());
+        context.addServlet(servletHolder, "/api/*");
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+
+
+
+    }
+
 }
--- src/main/java/dev/gether/anonymouse/rest/DemoRestController.java
+++ src/main/java/dev/gether/anonymouse/rest/DemoRestController.java
@@ -1,13 +1,86 @@
-package dev.gether.anonymouse;
+package dev.gether.anonymouse.rest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
-@Controller
-public class DemoRestController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-    @GetMapping("/")
-    public String getTest() {
-        return "123";
+public class DemoRestController extends HttpServlet {
+
+    // GET
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String urlPath = request.getPathInfo();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        // /API [register onEnable()]
+        // urlPath --->  /api/{urlPath}
+        switch (urlPath) {
+            case "/" -> {
+                response.getWriter().println("# "+urlPath);
+                return;
+            }
+            case "/temp" -> {
+                response.getWriter().println("#TEMP "+urlPath);
+                return;
+            }
+        }
+
+        response.getWriter().println("Wrong url "+urlPath);
+    }
+
+    // GET
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        String urlPath = request.getPathInfo();
+
+        switch (urlPath) {
+            case "/" -> {
+                String requestBody = getRequestData(request);
+                response.getWriter().println("# "+urlPath + " data: "+requestBody);
+                return;
+            }
+            case "/temp" -> {
+                String requestBody = getRequestData(request);
+                // if its JSON
+                //JSONObject json = new JSONObject(requestBody);
+                //String command = json.getString("KEY");
+                response.getWriter().println("#TEMP "+urlPath + " data: "+requestBody);
+                return;
+            }
+        }
+
+        // wrong post URL
+        // [!] OPTIONAL
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+
+        PrintWriter writer = response.getWriter();
+        writer.println("Wrong Post URL");
+
+    }
+
+    public String getRequestData(HttpServletRequest request) {
+        try {
+            BufferedReader reader = request.getReader();
+            StringBuilder requestBody = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                requestBody.append(line);
+            }
+
+            return requestBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
